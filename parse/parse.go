2c447c13662885024d82d458ea5b6c164f249fb1
Steven Ho <shteveno@berkeley.edu>
2017-11-27 08:07:55 -0800

    Somewhat working version of code

--- parse/parse.go
+++ parse/parse.go
@@ -21,7 +21,7 @@ type Node struct {
 }
 
 type State struct {
-    Node *Node
+    Node   *Node
     Stream []string
 }
 
@@ -29,6 +29,7 @@ var (
     lex  map[string][]string
     feat map[string][]string
     null map[string][]string
+    size int
 )
 
 // Waits for user input to process and parse into a tree.
@@ -44,11 +45,10 @@ func wait_user() string {
     }
 }
 
-// Attemps to merge the required specifier given the 
-// word stream. Returns all possible states where
-// the requested specifier can be interpreted
-func spec(stream []string, uFeat string, movedFeat string) []State {
-    //strings.Split(stream)
+// Attempts to merge the required complement given the
+// word stream. Returns all possible states where the
+// requested complement can be interpreted
+func comp(stream []string, uFeat string, strongFeat string) []State {
     var (
         head    string
         sel     string
@@ -72,12 +72,126 @@ func spec(stream []string, uFeat string, movedFeat string) []State {
                 bundles = feat[target]
             }
             for _, bundle := range bundles {
+                //fmt.Println("comp:", head, bundle)
                 feats := strings.Split(bundle, ",")
                 if feats[0] == sel {
                     xP := &Node{}
                     xP.Label = head + "P"
                     xP.Left = &Node{head, word, nil, nil, feats}
-                    states = append(states, State{xP, stream[i+1:]})
+                    wordsUsed := i+1
+                    if head != target {
+                        wordsUsed = i
+                    }
+                    if feats[2] != "." {
+                        comp_XPs := comp(stream[wordsUsed:], feats[2], "")
+                        if comp_XPs != nil {
+                            for _, comp_XP := range comp_XPs {
+                                xP.Right = comp_XP.Node
+                                states = append(states, State{xP, comp_XP.Stream})
+                            }
+                        }
+                    }
+                    if feats[1] != "." {
+                        spec_XPs := spec(stream[:i], feats[1])
+                        if spec_XPs != nil {
+                            for _, spec_XP := range spec_XPs {
+                                X_bar := xP
+                                X_bar.Label = head + "'"
+                                xP = &Node{}
+                                xP.Label = head + "P"
+                                xP.Left = spec_XP.Node
+                                xP.Right = X_bar
+                                states = append(states, State{xP, stream[wordsUsed:]})
+                                xP = X_bar
+                            }
+                        } else {
+                            continue
+                        }
+                    } else {
+                        states = append(states, State{xP, stream[wordsUsed:]})
+                    }
+                }
+            }
+        }
+    }
+    return states
+}
+
+// Attemps to merge the required specifier given the 
+// word stream. Returns all possible states where
+// the requested specifier can be interpreted
+func spec(stream []string, uFeat string) []State {
+    //strings.Split(stream)
+    var (
+        head      string
+        sel       string
+        states    []State
+        target    string   =  strings.Split(uFeat, "u")[1]
+        bundles   []string
+        wordsUsed int
+
+    )
+    for i, word := range stream {
+        categories := lex[word]
+        for _, cat := range categories {
+            cat_sel := strings.Split(cat, "_")
+            head = cat_sel[0]
+            sel = "."
+            if len(cat_sel) == 2 {
+                sel = cat_sel[1]
+            }
+            if head != target {
+                bundles = null[target]
+                wordsUsed = i
+            } else {
+                bundles = feat[target]
+                wordsUsed = i + 1
+            }
+            for _, bundle := range bundles {
+                offset := wordsUsed
+                feats := strings.Split(bundle, ",")
+                if feats[0] == sel {
+                    xP := &Node{}
+                    xP.Label = head + "P"
+                    xP.Left = &Node{head, word, nil, nil, feats}
+                    //wordsUsed := i+1
+                    //if head != target {
+                    //    wordsUsed = i
+                    //}
+                    if feats[2] != "." {
+                        //fmt.Println("run comp on:",feats[2], "and", stream[i:])
+                        comp_XPs := comp(stream[offset:], feats[2], "")
+                        if comp_XPs != nil {
+                            for _, comp_XP := range comp_XPs {
+                                xP.Right = comp_XP.Node
+                                offset += len(stream) - len(comp_XP.Stream)
+                                //states = append(states, State{xP, comp_XP.Stream})
+                                break
+                            }
+                        }
+                    }
+                    if feats[1] != "." {
+                        spec_XPs := spec(stream[:i], feats[1])
+                        if spec_XPs != nil {
+                            for _, spec_XP := range spec_XPs {
+                                X_bar := xP
+                                X_bar.Label = head + "'"
+                                xP = &Node{}
+                                xP.Label = head + "P"
+                                xP.Left = spec_XP.Node
+                                xP.Right = X_bar
+                                states = append(states, State{xP, stream[offset:]})
+                                xP = X_bar
+                            }
+                        } else {
+                            continue
+                        }
+                    } else {
+                        if (feats[1] != "." && xP.Left == nil) || (feats[1] == "." && feats[2] != "." && xP.Right == nil) || (feats[1] != "." && feats[2] != "." && xP.Right.Right == nil) {
+                            continue
+                        }
+                        states = append(states, State{xP, stream[offset:]})
+                    }
                 }
             }
         }
@@ -95,10 +209,11 @@ func parse(sentence string) *Node {
     t.Label = "T"
     t.Form = "$\\varnothing$"
     t.Features = []string{"FIN,*uD,uv"}
-    found := spec(stream, "*uD", "")
+    found := spec(stream, "*uD")
     if found == nil {
         return nil
     }
+    fmt.Println(len(found))
     fmt.Println("This is what remains:", found[0].Stream)
     ret := Node{}
     ret.Label = "TP"
